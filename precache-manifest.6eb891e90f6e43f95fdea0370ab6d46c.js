d547bfeb85ff0cf649ff26cd8589ec54d4181b3f
ranjanamrit <amrit.ranjan.work@gmail.com>
2020-07-22 00:00:06 +0530

    project added

---
+++ precache-manifest.6eb891e90f6e43f95fdea0370ab6d46c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a5f28d913bb50b71be4e3fbabcb9bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cfcd537b1d4580c1922",
+    "url": "/static/css/main.ba43963f.chunk.css"
+  },
+  {
+    "revision": "57b02bdf0e4fd90ca050",
+    "url": "/static/js/2.8f2935d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f2935d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfcd537b1d4580c1922",
+    "url": "/static/js/main.61873890.chunk.js"
+  },
+  {
+    "revision": "c33d7c8d0273dc6884f1",
+    "url": "/static/js/runtime-main.cf77074f.js"
+  },
+  {
+    "revision": "fc5d1e5c43f8e22e8839faf698294612",
+    "url": "/static/media/amrit.fc5d1e5c.png"
+  }
+]);
